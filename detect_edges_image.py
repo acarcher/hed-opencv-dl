3a55eba99a5c53446d9702a6aed058618e2d85a4
Alexander Archer <aca3936@gmail.com>
2019-03-04 20:17:31 -0500

    added CropLayer class

--- detect_edges_image.py
+++ detect_edges_image.py
@@ -11,3 +11,34 @@ ap.add_argument("-i", "--image", type=str, required=True,
                 help="path to input image")
 args = vars(ap.parse_args())
 
+
+class CropLayer(object):
+    def __init__(self, params, blobs):
+        # initialize our starting and ending (x, y)-coordinates of the crop
+        self.startX = 0
+        self.startY = 0
+        self.endX = 0
+        self.endY = 0
+
+    def getMemoryShapes(self, inputs):
+        # the crop layer will receive two inputs -- we need to crop
+        # the first input blob to match the shape of the second one,
+        # keeping the batch size and number of channels
+        (inputShape, targetShape) = (inputs[0], inputs[1])
+        (batchSize, numChannels) = (inputShape[0], inputShape[1])
+        (H, W) = (targetShape[2], targetShape[3])
+
+        # compute the starting and ending crop coordinates
+        self.startX = int((inputShape[3] - targetShape[3]) / 2)
+        self.startY = int((inputShape[2] - targetShape[2]) / 2)
+        self.endX = self.startX + W
+        self.endY = self.startY + H
+
+        # return the shape of the volume (we'll perform the actual
+        # crop during the forward pass)
+        return [[batchSize, numChannels, H, W]]
+
+    def forward(self, inputs):
+        # use the derviced (x, y)-coordinates to perform the crop
+        return [inputs[0][:, :, self.startY:self.endY,
+                                self.startX:self.endX]]
